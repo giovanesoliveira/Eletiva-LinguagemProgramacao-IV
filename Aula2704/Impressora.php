b1e5f570367902a749094b28956eed5f7b3b278a
giovanesoliveira <99844595+giovanesoliveira@users.noreply.github.com>
2022-04-27 20:42:55 -0300

    Update Impressora.php

--- Aula2704/Impressora.php
+++ Aula2704/Impressora.php
@@ -15,4 +15,34 @@ class Impressora {
     public function setMarca($marca){
         $this->marca = $marca;
     }
+
+
+    public function getModelo(){
+        return $this->modelo;
+    }
+
+
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+
+
+    public function getColorida(){
+        return $this->colorida;
+    }
+
+
+    public function setColorida($colorida){
+        $this->colorida = $colorida;
+    }
+
+
+    public function getPeso(){
+        return $this->peso;
+    }
+
+
+    public function setPeso($peso){
+        $this->peso = $peso;
+    }
 }
